2ffd0481fba2a47a3922f6a1f19d362e9cd93f8f
Martin McKeaveney <martin@squarefoot.com>
2019-03-18 00:44:39 +0000

    sorted dev server

--- server/index.js
+++ server/index.js
@@ -18,8 +18,8 @@ app.use(enableCorsMiddleware);
 if (process.env.NODE_ENV === 'development') {
   const webpack = require('webpack');
   const webpackDevMiddleware = require('webpack-dev-middleware');
-  const compiler = webpack(config);
   const config = require('../webpack.config.js');
+  const compiler = webpack(config);
 
   // Tell express to use the webpack-dev-middleware and use the webpack.config.js
   // configuration file as a base.
